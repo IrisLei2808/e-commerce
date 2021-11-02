6ce7de8675aefa12b3e3d7604350986d32fe0883
Nguyễn Quang Trung <gnaoh17939@gmail.com>
2021-11-02 08:11:58 +0700

    đổ data layou

--- src/views/products/product/CollectionItem.js
+++ src/views/products/product/CollectionItem.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 
-const CollectionItem = () => {
+const CollectionItem = ({ product }) => {
+  const defaultImage =
+    "https://cdn.tgdd.vn/Products/Images/42/228744/iphone-12-pro-max-512gb-191020-021035-200x200.jpg";
   return (
     <>
       <Card
@@ -15,24 +17,38 @@ const CollectionItem = () => {
         <Card.Text
           style={{ marginLeft: "50px", color: "orange", cursor: "pointer" }}
         >
-          Iphone 12 Pro Max 128GB
+          {product.name}
         </Card.Text>
-        <Card.Text style={{ marginLeft: "100px" }}>5 quantities</Card.Text>
+        <Card.Text style={{ marginLeft: "100px" }}>
+          {product.quantity} quantities
+        </Card.Text>
         <div className="img-product">
           <Card.Img
             variant="top"
             style={{ height: "67px", width: "67px", marginLeft: "20px" }}
-            src="https://cdn.tgdd.vn/Products/Images/42/228744/iphone-12-pro-max-512gb-191020-021035-200x200.jpg"
+            src={
+              product && product.Images[0]
+                ? product.Images[0].address
+                : defaultImage
+            }
           />
           <Card.Img
             variant="top"
             style={{ height: "67px", width: "67px", marginLeft: "20px" }}
-            src="https://cdn.tgdd.vn/Products/Images/42/228744/iphone-12-pro-max-512gb-191020-021035-200x200.jpg"
+            src={
+              product && product.Images[1]
+                ? product.Images[1].address
+                : defaultImage
+            }
           />
           <Card.Img
             variant="top"
             style={{ height: "67px", width: "67px", marginLeft: "20px" }}
-            src="https://cdn.tgdd.vn/Products/Images/42/228744/iphone-12-pro-max-512gb-191020-021035-200x200.jpg"
+            src={
+              product && product.Images[2]
+                ? product.Images[2].address
+                : defaultImage
+            }
           />
         </div>
       </Card>
--- src/views/products/product/ProductCollection.js
+++ src/views/products/product/ProductCollection.js
@@ -4,27 +4,16 @@ import { Row, Col } from "react-bootstrap";
 import CollectionItem from "./CollectionItem";
 
 const ProductCollection = ({ collection }) => {
-  let size = 8;
-  collection &&
-    collection.Categories.find((item) => {
-      item.Products.map((product) => {
-        console.log("EE: ", product);
-      });
-    });
   return (
     <>
       <h3>Trending {collection.brandname}</h3>
       <Row>
         {collection &&
-          collection.Categories.map(
-            (item) =>
-              item.Products.map((product) => {
-                <h1>{product.idProduct}</h1>;
-              })
-            // <Col sm={12} md={6} lg={4} xl={3} className="mb-4">
-            //   <CollectionItem products={collection.product} />
-            // </Col>
-          )}
+          collection.Products.map((product) => (
+            <Col sm={12} md={6} lg={4} xl={3} className="mb-4">
+              <CollectionItem product={product} />
+            </Col>
+          ))}
       </Row>
     </>
   );
